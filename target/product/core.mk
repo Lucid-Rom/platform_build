eb112eef065ee2a24146cf2f6f0d21587241ab97
Michael J. Cusack <WhoNeedszZz@gmail.com>
2015-07-02 02:00:44 -0500

    Remove unnecessary AOSP packages
    Also change default tone settings
    
    Change-Id: I305a86a54cb2c18f5abc38493130a2ce80c1b669
    Signed-off-by: Michael J. Cusack <WhoNeedszZz@gmail.com>

--- target/product/core.mk
+++ target/product/core.mk
@@ -21,10 +21,7 @@
 # core_tiny.mk in addition to core_minimal.mk.
 
 PRODUCT_PACKAGES += \
-    BasicDreams \
-    Browser \
     Calculator \
-    Calendar \
     CalendarProvider \
     CaptivePortalLogin \
     CertInstaller \
@@ -33,7 +30,6 @@ PRODUCT_PACKAGES += \
     DocumentsUI \
     DownloadProviderUi \
     Email \
-    Exchange2 \
     ExternalStorageProvider \
     FusedLocation \
     InputDevices \
@@ -42,7 +38,6 @@ PRODUCT_PACKAGES += \
     LatinIME \
     Launcher2 \
     ManagedProvisioning \
-    PicoTts \
     PacProcessor \
     libpac \
     PrintSpooler \
